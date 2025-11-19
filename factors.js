1d6d4e1ad19e928142b5c5385f0af7074ba37cc8
dncworking <dominyka.dobuzinskaite@gmail.com>
2025-11-19 16:49:41 +0200

    add factors.js / work done

--- factors.js
+++ factors.js
@@ -1,2 +1,12 @@
 "use strict";
 
+function factors(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+console.log(factors(12));
